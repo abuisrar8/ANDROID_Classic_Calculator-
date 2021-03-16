9afb0ecf1c8d038c9e9a6f80d43aba356aa4e7a0
Israr <abu.israr.khan.9@gmail.com>
2021-03-16 21:23:07 +0530

    on click (any) - > text view on click backSpace >> on click Clear >> on click dot - > functionality-> view one Dot only

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -11,13 +11,49 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var isNewOperator = true
+    lateinit var textView:TextView
+    var lastNum : Boolean =false
+    var lastDot : Boolean =false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView  = findViewById<TextView>(R.id.editText)
+        lastNum =true
+
     }
 
     fun onDigit (view : View){
-        Toast.makeText(this,"Button Works", Toast.LENGTH_LONG).show()
+        textView.append((view as Button).text)
+
+    }
+
+    fun onClear(view: View ){
+        textView.text =""
+        lastNum =true
+        lastDot =false
+
+    }
+
+    fun onBackSpace(view : View){
+        var s = textView.text.toString()
+        if(s != ""){
+            if(s[s.length-1] == '.'){
+                lastNum =true
+                lastDot =false
+            }
+            s= s.substring(0,s.length-1)
+            textView.text = s
+        }
+
+    }
+
+    fun onDecimalPoint(view :View){
+        if(lastNum && !lastDot){
+            textView.append(".")
+            lastNum =false
+            lastDot =true
+        }
     }
 }
 
